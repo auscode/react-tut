4c8c21f5565823a0e5481da8f96f90b06ee8c49d
auscode <harshittext@gmail.com>
2023-11-27 17:01:53 +0530

    adding form and slug values

--- 12MegaBlog/src/store/store.js
+++ 12MegaBlog/src/store/store.js
@@ -6,3 +6,18 @@ const store = configureStore({
 });
 
 export default store;
+
+
+
+// import {configureStore} from '@reduxjs/toolkit';
+// import authSlice from './authSlice';
+
+// const store = configureStore({
+//     reducer: {
+//         auth : authSlice,
+//         //TODO: add more slices here for posts
+//     }
+// });
+
+
+// export default store;
